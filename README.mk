fdee3aacfedc0fdf7e0848b1a7fb9d8f6b9b4a5e
Nathan <nathan.bolam@fairfaxmedia.com.au>
2019-03-01 08:18:19 +1100

    Update README.mk

--- README.mk
+++ README.mk
@@ -1,6 +1,6 @@
-src/gz base https://raw.githubusercontent.com/BoLaMN/brcm63xx-tch/master/packages/base
+echo "src/gz base https://raw.githubusercontent.com/BoLaMN/brcm63xx-tch/master/packages/base
 src/gz luci https://raw.githubusercontent.com/BoLaMN/brcm63xx-tch/master/packages/luci
 src/gz management https://raw.githubusercontent.com/BoLaMN/brcm63xx-tch/master/packages/management
 src/gz packages https://raw.githubusercontent.com/BoLaMN/brcm63xx-tch/master/packages/packages
 src/gz routing https://raw.githubusercontent.com/BoLaMN/brcm63xx-tch/master/packages/routing
-src/gz telephony https://raw.githubusercontent.com/BoLaMN/brcm63xx-tch/master/packages/telephony
+src/gz telephony https://raw.githubusercontent.com/BoLaMN/brcm63xx-tch/master/packages/telephony" > /etc/opkg/distfeeds.conf
